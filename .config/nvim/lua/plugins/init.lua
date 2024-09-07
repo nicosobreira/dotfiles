3e9454aee5056786cf066defe7cb05c563d7e3c3
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-09-07 18:12:20 -0300

    Nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,20 @@
 return {
   {
+    "alexghergh/nvim-tmux-navigation", config = function()
+    require'nvim-tmux-navigation'.setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+            left = "<C-h>",
+            down = "<C-j>",
+            up = "<C-k>",
+            right = "<C-l>",
+            last_active = "<C-\\>",
+            next = "<C-Space>",
+        }
+    }
+end
+  },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
