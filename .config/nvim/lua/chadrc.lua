d1072407d1c9e4ecfbe8cba03b802e51979982bc
nicosobreira <nicolau.sobreira@gmail.com>
2024-11-12 10:38:59 -0300

    Move ensure installed to `chadrc`

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,22 +1,35 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "chadracula-evondev",
+  theme = "chadracula-evondev",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+
+M.mason.pkgs = {
+  "clangd",
+  "codelldb",
+  "css-lsp",
+  "debugpy",
+  "html-lsp",
+  "js-debug-adapter",
+  "lua-language-server",
+  "pyright",
+  "stylua",
+  "typescript-language-server",
 }
 
 M.ui = {
   tabufline = {
-    enabled = false
-  }
+    enabled = false,
+  },
 }
 
 return M
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,18 +1,18 @@
 return {
   "williamboman/mason.nvim",
   lazy = true,
-  opts = {
-    ensure_installed = {
-      "clangd",
-      "codelldb",
-      "css-lsp",
-      "debugpy",
-      "html-lsp",
-      "js-debug-adapter",
-      "lua-language-server",
-      "pyright",
-      "stylua",
-      "typescript-language-server",
-    },
-  },
+  -- opts = {
+  --   ensure_installed = {
+  --     "clangd",
+  --     "codelldb",
+  --     "css-lsp",
+  --     "debugpy",
+  --     "html-lsp",
+  --     "js-debug-adapter",
+  --     "lua-language-server",
+  --     "pyright",
+  --     "stylua",
+  --     "typescript-language-server",
+  --   },
+  -- },
 }
