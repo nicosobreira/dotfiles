e0ac98e113b28fb5bf07c11d9500d9683b268407
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-12-17 17:23:46 -0300

    Add TermOpen

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,16 +1,26 @@
 require "nvchad.options"
 
--- add yours here!
-
 local opt = vim.opt
+local autocmd = vim.api.nvim_create_autocmd
 
--- o.tabstop = 4
--- o.shiftwidth = 4
--- o.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 
+-- Options --
 opt.relativenumber = true
 opt.scrolloff = 5
 opt.breakindent = true
 opt.linebreak = true
 opt.swapfile = false
 opt.spell = true
+
+-- Auto commands --
+autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    opt.number = false
+    opt.relativenumber = false
+    opt.spell = false
+  end,
+})
