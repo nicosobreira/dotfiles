d49885cc60f6329264cbac654b38f22bfbf09bd2
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-12-19 20:01:02 -0300

    Fix nvlsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ return {
 
     local lspconfig = require "lspconfig"
     local nvlsp = require "nvchad.configs.lspconfig"
+    nvlsp.defaults()
 
     local servers = { "ts_ls", "html", "cssls", "pyright", "clangd", "bashls" }
 
@@ -14,25 +15,17 @@ return {
         on_attach = nvlsp.on_attach,
         on_init = nvlsp.on_init,
         capabilities = nvlsp.capabilities,
-        -- init_options = {
-        --   preferences = {
-        --     disableSuggestions = true,
-        --   },
-        -- },
       }
     end
+
     lspconfig.clangd.setup {
-      -- Optional: Enable clangd-specific settings
-      -- cmd = { "clangd", "--background-index", "--compile-commands-dir=build" },
       settings = {
         clangd = {
-          -- Additional clangd settings
           diagnostics = true,
         },
       },
     }
 
-    -- configuring single server, example: typescript
     lspconfig.pyright.setup {
       settings = {
         python = {
