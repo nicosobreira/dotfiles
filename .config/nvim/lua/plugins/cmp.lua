296df3672e5d70d49c511005b962820cfd4060d9
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-12-19 20:01:15 -0300

    Add cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,4 +1,24 @@
+local cmp = require "cmp"
+
 return {
   "hrsh7th/nvim-cmp",
-  enabled = false,
+  enable = false,
+  opts = function()
+    local conf = require "nvchad.configs.cmp"
+
+    local my_mappings = {
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-y>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      },
+    }
+    conf.mapping = my_mappings
+    return conf
+  end,
+  config = function()
+    cmp.setup { enabled = false }
+  end,
 }
