e7a5150384a06e13fac3671bc250c1032935e1eb
nicosobreira <nicolau.sobreira@gmail.com>
2025-02-08 08:42:41 -0300

    Remove comments

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -21,6 +21,7 @@ return {
 				},
 			},
 			pyright = {
+				filetype = { "python" },
 				settings = {
 					python = {
 						analysis = {
@@ -39,31 +40,5 @@ return {
 			opts.capabilities = nvlsp.capabilities
 			lspconfig[name].setup(opts)
 		end
-
-		-- lspconfig.lua_ls.setup({
-		-- 	on_attach = nvlsp.on_attach,
-		-- 	on_init = nvlsp.on_init,
-		-- 	capabilities = nvlsp.capabilities,
-		-- 	settings = {
-		-- 		Lua = {
-		-- 			diagnostics = {
-		-- 				disable = { "missing-parameter" },
-		-- 				globals = { "vim" },
-		-- 			},
-		-- 			workspace = {
-		-- 				library = {
-		-- 					vim.fn.expand("$VIMRUNTIME/lua"),
-		-- 					vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-		-- 					vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
-		-- 					vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
-		-- 					"${3rd}/luv/library",
-		-- 				},
-		-- 				maxPreload = 100000,
-		-- 				preloadFileSize = 10000,
-		-- 			},
-		-- 		},
-		-- 	},
-		-- })
-		lspconfig.pyright.setup({})
 	end,
 }
