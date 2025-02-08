1129a74cf1c290daafd4d3d0b08f61b0dfa206e4
nicosobreira <nicolau.sobreira@gmail.com>
2025-02-08 08:41:57 -0300

    Remove comments

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -5,32 +5,18 @@
 local M = {}
 
 M.base46 = {
-  theme = "chadracula-evondev",
+	theme = "chadracula-evondev",
 
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-  },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
--- M.mason.pkgs = {
---   "clangd",
---   "codelldb",
---   "css-lsp",
---   "debugpy",
---   "html-lsp",
---   "js-debug-adapter",
---   "lua-language-server",
---   "pyright",
---   "stylua",
---   "typescript-language-server",
---   "bash-language-server",
--- }
-
 M.ui = {
-  tabufline = {
-    order = { "treeOffset", "tabs" },
-  },
+	tabufline = {
+		order = { "treeOffset", "tabs" },
+	},
 }
 
 return M
