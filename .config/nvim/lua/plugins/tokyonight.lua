7745c1beb72e5dd372ad533479c068a8ddbc9b94
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-08 14:18:19 -0300

    Simpler load

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -3,7 +3,6 @@ return {
 	lazy = false, -- make sure the colorscheme is loaded immediately
 	priority = 1000, -- load this before all other plugins
 	config = function()
-		vim.cmd.colorscheme('tokyonight')
-		-- You can add additional theme configuration here
+		require("tokyonight").load()
 	end,
 }
