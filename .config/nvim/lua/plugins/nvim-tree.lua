4e5aa6f03c8c9d574602a891c6de9029a4b7272a
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-07 22:27:35 -0300

    Remove neovim config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-tree/nvim-tree.lua",
-  opts = {
-    filters = { dotfiles = false },
-  },
-  keys = {
-    { "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "Toggle nvim-tree" },
-  },
-}
--- .config/nvim/lua/plugins/tmux-navigator.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "christoomey/vim-tmux-navigator",
-  event = function()
-    if vim.fn.exists "$TMUX" == 1 then
-      return "VeryLazy"
-    end
-  end,
-}
