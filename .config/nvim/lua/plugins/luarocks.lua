4207db8b271597dd9aedefb47e97c3729d49ea5a
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-12 14:04:59 -0300

    disable luarocks

--- .config/nvim/lua/plugins/luarocks.lua
+++ .config/nvim/lua/plugins/luarocks.lua
@@ -1,5 +1,6 @@
 return {
 	"vhyrro/luarocks.nvim",
+	enabled = false,
 	priority = 2000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
 	config = true,
 }
