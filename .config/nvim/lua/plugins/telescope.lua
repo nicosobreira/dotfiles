ddc1fbae30905b81b0f88225a6c5485a2908b79c
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-12 14:05:11 -0300

    Add fzf native plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,11 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	-- tag = "0.1.8",
+	dependencies = {
+		{ 'nvim-lua/plenary.nvim' },
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
 	cmd = { "Telescope" },
 	keys = {
 		{
