95e2ac07b5edcffabdef270252dd22453d461e03
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-13 13:48:52 -0300

    Don't lazy load treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	version = false,
 	build = ":TSUpdate",
-	lazy = vim.fn.argc(-1) == 0, -- load early when opend from cmd
 	init = function(plugin)
 		require("lazy.core.loader").add_to_rtp(plugin)
 		require("nvim-treesitter.query_predicates")	
 	end,
 	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-	opts_extend = { "ensure_installed" },
-	---@type TSConfig
-	---@diagnostic disable-next-line: missing-fields
 	opts = {
 		highlight = { enable = true },
 		indent = { enable = true },
@@ -41,7 +38,6 @@ return {
 			"yaml",
 		},
 	},
-	---@param opts TSConfig
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
 	end,
