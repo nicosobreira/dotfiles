619e101471d2d931886deb0a25e35f9fe75c8bbf
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-20 17:22:08 -0300

    Add clangd lsp options

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -190,7 +190,13 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			clangd = {},
+			clangd = {
+				cmd = {
+					"clangd",
+					"--background-index", -- Better for large projects
+					"--header-insertion=never", -- Avoid auto-adding headers
+				},
+			},
 			bashls = {},
 			-- gopls = {},
 			-- pyright = {},
