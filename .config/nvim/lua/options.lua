c92c55f82935f4c1edf98874328d8f2c97c8c8f7
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-28 19:26:46 -0300

    Using "" and new options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,5 +1,5 @@
 -- Initialize with default options
-vim.opt.background = 'dark'
+vim.opt.background = "dark"
 vim.opt.termguicolors = true
 vim.cmd("filetype plugin indent on")
 vim.cmd("syntax enable")
@@ -7,15 +7,19 @@ vim.cmd("syntax enable")
 -- Basic settings
 vim.opt.wrap = true
 vim.opt.linebreak = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.autoread = true
-vim.opt.undofile = true
-vim.opt.undolevels = 10000
-vim.opt.encoding = 'utf-8'
+vim.opt.encoding = "utf-8"
 vim.opt.showmode = false
+vim.opt.scrolloff = 10
+vim.opt.backspace = {"indent", "eol", "start"}
+
+-- Undo
 vim.opt.backup = false
 vim.opt.swapfile = false
-vim.opt.scrolloff = 10
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
 
 -- "Ominfunc" help size
 vim.opt.pumheight = 10
@@ -24,7 +28,7 @@ vim.opt.pumheight = 10
 vim.opt.conceallevel = 2
 
 -- Spell check
-vim.opt.spelllang = {'en', 'pt_br'}
+vim.opt.spelllang = {"en", "pt_br"}
 
 -- Indentation
 vim.opt.breakindent = true
@@ -35,13 +39,13 @@ vim.opt.smartindent = true
 
 -- Visual whitespace
 vim.opt.list = true
-vim.opt.listchars = { tab = '| ', trail = '*' }
+vim.opt.listchars = { tab = "| ", trail = "*" }
 
 -- Clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard:append("unnamedplus")
 
 -- Auto comment
-vim.opt.formatoptions = 'cro'
+vim.opt.formatoptions = "cro"
 
 -- Status line
 vim.opt.laststatus = 2
@@ -67,11 +71,11 @@ vim.opt.splitright = true
 
 -- Wildmenu
 vim.opt.wildmenu = true
-vim.opt.wildmode = 'longest:full,full'
-vim.opt.wildoptions = 'fuzzy'
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildoptions = "fuzzy"
 
 -- Netrw settings
-vim.g.netrw_list_hide = '^\\./$,^\\.\\./$'
+vim.g.netrw_list_hide = "^\\./$,^\\.\\./$"
 vim.g.netrw_browse_split = 0
 vim.g.netrw_hide = 1
 vim.g.netrw_banner = 0
@@ -79,7 +83,7 @@ vim.g.netrw_altfile = 1
 vim.g.netrw_alto = 1
 
 -- Cursor shape
-vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
+vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 
 -- Folding
 vim.opt.foldmethod = "syntax"
