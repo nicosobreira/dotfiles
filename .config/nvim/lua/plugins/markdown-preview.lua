399845fc8423703fe0ade67658cf2435682e7dce
nicosobreira <nicolau.sobreira@gmail.com>
2025-04-29 09:21:47 -0300

    Markdown stuff

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,11 +1,11 @@
 return {
 	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function() vim.fn["mkdp#util#install"]() end,
 	cond = function()
 		return vim.fn.executable("nodejs") == 1
 	end,
+	ft = { "markdown" },
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = function() vim.fn["mkdp#util#install"]() end,
 	keys = {
 		{
 			"<leader>.",
