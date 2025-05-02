8733127268e342b24c6fb0961e21ffa7920c2248
nicosobreira <nicolau.sobreira@gmail.com>
2025-05-01 21:11:08 -0300

    Using catppuccin

--- .config/nvim/lua/plugins/tokyonight.lua
+++
@@ -1,8 +0,0 @@
-return {
-	'folke/tokyonight.nvim',
-	lazy = false, -- make sure the colorscheme is loaded immediately
-	priority = 1000, -- load this before all other plugins
-	config = function()
-		require("tokyonight").load()
-	end,
-}
