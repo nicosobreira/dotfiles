7588223f4cde7763e05b01cf1a80216aaf1bc718
nicosobreira <nicolau.sobreira@gmail.com>
2025-05-05 18:19:25 -0300

    Rename to wallpapers

--- suckless/startdwm.sh
+++ suckless/startdwm.sh
@@ -5,11 +5,11 @@ xset r rate 190 50
 
 [ -f "$HOME/.Xresources" ] && xrdb -merge -I"$HOME" "$HOME/.Xresources"
 
-feh --bg-fill "$HOME/suckless/wallpaper/wallpaper2.png" &
+feh --bg-fill "$HOME/suckless/wallpapers/wallpaper2.png" &
 
 setxkbmap -model pc105 -layout br -variant abnt2
 
-setsid "$HOME/suckless/scripts/bar.sh" &> /dev/null &
+dwmblocks &> /dev/null &
 
 while true; do
 	dwm 2> "$HOME/.dwm.log"
