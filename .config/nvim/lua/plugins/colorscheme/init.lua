889c3567bb478240948aa1a137e38fedcfc7933c
nicosobreira <nicolau.sobreira@gmail.com>
2026-01-26 23:49:13 -0300

    Better coloscheme

--- .config/nvim/lua/plugins/colorscheme/init.lua
+++ .config/nvim/lua/plugins/colorscheme/init.lua
@@ -7,6 +7,14 @@ return {
 
 		require("base16-pro-max").setup({
 			colors = colors,
+			styles = {
+				italic = true,
+				bold = true,
+				transparency = true,
+			},
+			plugins = {
+				enable_all = true,
+			},
 			highlight_groups = {
 				["StatusLine"] = {
 					fg = colors.base01,
@@ -23,40 +31,37 @@ return {
 					bg = colors.base00,
 				},
 
-				["@markup.strong"] = {
-					bold = true,
-				},
-				["@markup.italic"] = {
-					italic = true
+				["@attribute"] = {
+					fg = colors.base0E,
 				},
 
 				["@markup.heading"] = {
 					fg = colors.base06,
-					bg = colors.base00
+					bg = colors.base00,
 				},
 				["@markup.heading.1"] = {
 					fg = colors.base08,
-					bg = colors.base00
+					bg = colors.base00,
 				},
 				["@markup.heading.2"] = {
 					fg = colors.base09,
-					bg = colors.base00
+					bg = colors.base00,
 				},
 				["@markup.heading.3"] = {
 					fg = colors.base0B,
-					bg = colors.base00
+					bg = colors.base00,
 				},
 				["@markup.heading.4"] = {
 					fg = colors.base0D,
-					bg = colors.base00
+					bg = colors.base00,
 				},
 				["@markup.heading.5"] = {
 					fg = colors.base0E,
-					bg = colors.base00
+					bg = colors.base00,
 				},
-			}
+			},
 		})
 
 		vim.cmd.colorscheme("base16-pro-max")
-  end,
+	end,
 }
