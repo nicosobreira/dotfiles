3721988e7448f66b1dc4342e0944f0ef343a9642
nicosobreira <nicolau.sobreira@gmail.com>
2026-03-03 12:30:35 -0300

    Unecessary ui_utils

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -8,8 +8,6 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
-local ui_utils = require("ui.utils")
-
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
