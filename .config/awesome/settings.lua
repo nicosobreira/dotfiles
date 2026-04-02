835556d714423c07a6a4e5f693b43ba3f138d828
nicosobreira <nicolau.sobreira@gmail.com>
2026-04-02 18:40:40 -0300

    Remove unecessary fix

--- .config/awesome/settings.lua
+++ .config/awesome/settings.lua
@@ -6,7 +6,6 @@ return {
 	increase_window_factor = 0.05,
 	spacing = dpi(0),
 
-	-- FIX: `os.getenv("TERMINAL") doesn't work?
 	terminal = os.getenv("TERMINAL") or "kitty",
 	editor = os.getenv("EDITOR") or "nvim",
 	browser = os.getenv("BROWSER") or "brave",
