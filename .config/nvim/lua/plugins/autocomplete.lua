637c8b4d6c09ea0d4aed5f625d50661b30f3dd6b
nicosobreira <nicolau.sobreira@gmail.com>
2026-05-27 17:23:24 -0300

    Add autocomplete with cmp and luasnip

---
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,68 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+
+			completion = {
+				autocomplete = false,
+			},
+
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping(function(fallback)
+					local has_words_before = function()
+						unpack = unpack or table.unpack
+						local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+						return col ~= 0
+							and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+								== nil
+					end
+
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+
+			sources = cmp.config.sources({
+				{ name = "luasnip" },
+				{ name = "nvim_lsp" },
+			}),
+		})
+	end,
+}
