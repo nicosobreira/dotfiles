b72f4f74b23db54fc3ffe12e422971d8150f2547
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-22 19:38:57 -0300

    chore: removing `pomodoro` widget

--- .config/awesome/widgets/init.lua
+++ .config/awesome/widgets/init.lua
@@ -2,7 +2,6 @@ return {
 	battery = require("widgets.battery"),
 	volume = require("widgets.volume"),
 	bright = require("widgets.bright"),
-	pomodoro = require("widgets.pomodoro"),
 	calendar = require("widgets.calendar"),
 	time = require("widgets.time"),
 }
