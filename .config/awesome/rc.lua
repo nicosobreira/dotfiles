36fb98050e74d5d6e0b16c0c83c5348d19e1a958
nicosobreira <nicolau.sobreira@gmail.com>
2026-06-22 19:41:30 -0300

    chore: remove unused `hotkeys_popup` require

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -6,7 +6,6 @@ require("awful.autofocus")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
-local hotkeys_popup = require("awful.hotkeys_popup")
 
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
